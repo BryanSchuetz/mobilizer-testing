b9b25a2990bcb23f7024028a7c4e14d1fa071860
Bryan Schuetz <bschuetz@gmail.com>
2012-04-26 18:23:31 -0400

    more better

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-Testing mobilizer deployment here
+###Mobilizer
+
+A mobile optimized web app for soliciting public outreach to Capitol Hill. Intended for those wanting to direct public reaction to congress around a particular piece of legislation currently under debate. Just swap in your own content blocks (the legislation, the argument, the talking points, the bacground) and deploy.
+
+- - - - 
+####Demo
+
+
+- - - - 
+* Uses the [Sunlight Congress API](http://services.sunlightlabs.com/docs/Sunlight_Congress_API/) to pull representatives associated with a 5 digit Zip Code.
+	* First Name, Last Name, Title, Phone, Twitter ID, Picture: All maintained by the Sunlight Foundation
+* Built as a Rack App so it's super easy to deploy to any Ruby friendly host.
+* The app is a "Responsive" design but _NOT_ fluid, because fluid layouts are the devil.
+	* If you're not sure what "Responsive Web Design" is, it's essentially a design that allows the content on the page to adapt as the context with which it's viewed changes. So as the browser viewport gets smaller, as when viewing the page on a mobile phone for instance, the layout changes to better suit that context. Try resizing your browser window in the demo and you'll get the idea. [Learn more](http://www.alistapart.com/articles/responsive-web-design/).
+
+- - - - 
+
+####Still To Do
+
+* Move content blocks into markdown partials for easier customization
+* Clean up the SASS and make basic styling: color scheme, fonts, etc. easier to customize.
+* Improve ZIP Code validation (database?)
+
--- public/js/core.js
+++ public/js/core.js
@@ -1,12 +1,14 @@
+//ZipCode Validation
 function validateZIP(field) {
-
+//Offset for box-2 with errors in wide layout
 var marg = $("#box-2").css("margin-top");
-if (marg == "245px"){
+if (marg == "256px"){
 	$("#box-2").css("margin-top", "276px");
 }
-if (marg == "223px"){
+if (marg == "233px"){
 	$("#box-2").css("margin-top", "256px");
 }
+//Check for Valid Zip Format
 function isValidZip(ZIP) {
     var pattern = new RegExp(/(^\d{5}$)|(^\d{5}-\d{4}$)/);
     return pattern.test(ZIP);
@@ -15,13 +17,13 @@ if (!isValidZip (field)){
 	$('#errorZip').html('*Not A Valid ZIP Code*');
 	return false;
 }
-
+//Check for DC zip
 if ((field == "20012") ||(field =="20015") ||(field =="20011") ||(field =="20016") ||(field =="20008") ||(field =="20010") ||(field =="20017") ||(field =="20007") ||(field =="20009") ||(field =="20001") ||(field =="20002") ||(field =="20018") ||(field =="20037") ||(field =="20036") ||(field =="20005") ||(field =="20006") ||(field =="20019") ||(field =="20024") ||(field =="20003") ||(field =="20319") ||(field =="20020") ||(field =="20375") ||(field =="20032")){
 $('#errorZip').html('*No taxation without representation.*');
 $('#zip').focus();
 return false;
 }
-
+//Check for 5+4 format
 if (field.length>5) {
 $('#errorZip').html('*We only want the 5 digit version*');
 $('#zip').focus();
@@ -30,3 +32,19 @@ return false;
 
 return true;
 }
+//Hide Address Bar in Mobile Safari
+function hideAddressBar()
+{
+  if(!window.location.hash)
+  {
+      if(document.height < window.outerHeight)
+      {
+          document.body.style.height = (window.outerHeight + 50) + 'px';
+      }
+
+      setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
+}
+
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
